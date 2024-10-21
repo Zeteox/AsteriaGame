1575e70cd5388936262dcaa461ccac93b1ffdc92
yland <ylan.dessenne@ynov.com>
2024-10-21 17:19:13 +0200

    potions class finish

--- potion.h
+++ potion.h
@@ -1,10 +1,25 @@
 #ifndef POTION_H
 #define POTION_H
 
-class Potion
-{
+#include <iostream>
+
+using namespace std;
+
+class Potion {
+    string m_name;
+    int m_hp;
+    int m_price;
 public:
-    Potion();
+    Potion(int level);
+    string& getName();
+    void setName(string name);
+    void setHp(int hp);
+    int getHp();
+    void setPrice(int price);
+    int getPrice();
+    void createPotion(int level);
 };
 
-#endif // POTION_H
+
+
+#endif //POTION_H
