7f8adcc2e0f7d3abf8b66b82367e7d236116a0d3
Delprat Loïc <loic.delprat@ynov.com>
2024-11-04 16:16:34 +0100

    deleted tests in main

--- main.cpp
+++ main.cpp
@@ -11,23 +11,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    // ----------test
-    vector<Potion*> inv;
-    for (int x=0;x<4;x++) {
-        inv.push_back(new Potion(1));
-    }
-    qDebug()<<inv.size();
-    qDebug()<<inv[2];
-    auto nx= next(inv.begin()+1);
-    inv.erase(nx);
-    qDebug()<<inv.size();
-    qDebug()<<inv[2];
-    nx= next(inv.begin()+1);
-    inv.erase(nx);
-    qDebug()<<inv.size();
-    qDebug()<<inv[2];
-    // -----------
-
     return a.exec();
 }
