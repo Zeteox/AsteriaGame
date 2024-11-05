d199939a1c2f295ddec90fb4b7385cb9a82927b9
yland <ylan.dessenne@ynov.com>
2024-11-05 11:13:21 +0100

    deleted test in main

--- main.cpp
+++ main.cpp
@@ -8,7 +8,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    Village* bourg = new Village("bourg");
-    qDebug() << "village : " << bourg->getAllBuildings();
     return a.exec();
 }
