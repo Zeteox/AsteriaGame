fb94c6609f4e1200ddbdf0c517ce4eba3b8f7ede
yland <ylan.dessenne@ynov.com>
2024-11-07 00:41:16 +0100

    weapon vector in Inventory class added

--- inventory.h
+++ inventory.h
@@ -2,19 +2,24 @@
 #define INVENTORY_H
 #include <vector>
 #include "potion.h"
+#include "weapon.h"
 
 class Inventory
 {
 private:
     size_t m_space;
     vector<Potion*> m_potions;
+    vector<Weapon*> m_weapons;
 public:
     Inventory();
     size_t getSpace();
+    string addSpace();
     vector<Potion*> showPotions();
     string addPotion(Potion* potion);
     string removePotion(Potion* potion);
-    string addSpace();
+    vector<Weapon*> showWeapons();
+    string addWeapon(Weapon* weapon);
+    string removeWeapon(Weapon* weapon);
 };
 
 #endif // INVENTORY_H
